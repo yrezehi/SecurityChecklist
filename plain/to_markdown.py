835200792bcafb24f2ddac314737ace7c8a9d65a
unknown <yrezehi@gmail.com>
2023-09-28 22:54:39 -0700

    enhance: to_markdown

--- plain/to_markdown.py
+++ plain/to_markdown.py
@@ -5,9 +5,25 @@ folder = "./"
 
 def read_file(file_path):
     with open(file_path) as file:
-        return file.read() 
+        return [line.rstrip('\n') for line in file]
 
 def get_file_path(file_name):
     return folder + file_name
 
-print(read_file(get_file_path(files[0])))
+def file_to_markdown_table(file_name):
+    return to_markdown_table(read_file(get_file_path(file_name)))
+
+def to_markdown_table(content):
+    lines = []
+    for content_line in content:
+        lines.append("| " + content_line + " |")
+    return "\n".join(lines)
+
+def build_header():
+    return "| Issue |\n| ------------- |\n"
+
+markdown_content = []
+
+for file_name in files:
+    markdown_content.append(build_header() + file_to_markdown_table(file_name))
+
