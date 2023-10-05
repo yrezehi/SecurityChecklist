149dee74dfe671b4f3b19e93e3da3dcf023ea75b
unknown <yrezehi@gmail.com>
2023-10-05 12:54:01 -0700

    setup: caches

---
+++ WebApplication/WebApplication/Cache/Cache.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System.Collections.Concurrent;
+
+namespace Application.Cache
+{
+    public class Cache
+    {
+        private readonly ConcurrentDictionary<string, MemoryCache> Caches;
+
+        public Cache() =>
+            Caches = new ConcurrentDictionary<string, MemoryCache>();
+        
+        public MemoryCache GetCache(string name)
+        {
+            if (Caches.ContainsKey(name))
+            {
+                return Caches[name];
+            }
+
+            throw new ArgumentException($"{name} is not initialized.");
+        }
+
+    }
+}
