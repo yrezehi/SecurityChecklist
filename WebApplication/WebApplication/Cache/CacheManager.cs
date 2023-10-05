ba8a8d958500a0fee2133fb4f687e42f506aedb6
unknown <yrezehi@gmail.com>
2023-10-05 13:13:49 -0700

    add: crd to cachje manager

--- WebApplication/WebApplication/Cache/CacheManager.cs
+++ WebApplication/WebApplication/Cache/CacheManager.cs
@@ -10,13 +10,14 @@ namespace Application.Cache
 
         public CacheManager() =>
             Caches = new ConcurrentDictionary<string, MemoryCache>();
-        
+
+        private MemoryCache CreateMemeoryCacheInstance() =>
+            new MemoryCache(new MemoryCacheOptions());
+
         public MemoryCache GetCache(string name)
         {
             if (Caches.ContainsKey(name))
-            {
                 return Caches[name];
-            }
 
             throw new ArgumentException($"{name} is not initialized.");
         }
@@ -27,8 +28,16 @@ namespace Application.Cache
                 throw new ArgumentException($"Was not able to create a cache instance for {name}");
         }
 
-        private MemoryCache CreateMemeoryCacheInstance() =>
-            new MemoryCache(new MemoryCacheOptions());
+        public bool Contains(string name, string key) =>
+            this.GetCache(name).TryGetValue(key, out _);
 
+        public void Remove(string name, string key) =>
+            this.GetCache(name).Remove(key);
+
+        public void Set<T>(string name, string key, T value) =>
+            this.GetCache(name).Set(key, value);
+
+        public T? Get<T>(string name, string key) =>
+            this.GetCache(name).TryGetValue(key, out T? value) ? value : default;
     }
 }
