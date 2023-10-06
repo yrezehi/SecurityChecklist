c56cdb648f5b35b6151ef71e5870a68b65ed1f98
unknown <yrezehi@gmail.com>
2023-10-06 10:33:40 -0700

    logic: rate limit

--- WebApplication/WebApplication/Events/RateLimitEventHandler.cs
+++ WebApplication/WebApplication/Events/RateLimitEventHandler.cs
@@ -1,12 +1,27 @@
-﻿namespace Application.Events
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Application.Events
 {
     public class RateLimitEventHandler : IMiddleware
     {
-        public RateLimitEventHandler() { }
+        private static string PAGE_LIMIT_URI = "/GOTOCD";
 
         public async Task InvokeAsync(HttpContext context, RequestDelegate next)
         {
-            await next(context);
+            if (IsRateLimitExceeded(context))
+            {
+                SetRedirectToException(context);
+            } else
+            {
+                await next(context);
+            }
         }
+
+        private bool IsRateLimitExceeded(HttpContext context) =>
+            true;
+
+        private void SetRedirectToException(HttpContext context) =>
+            context.Response.Redirect(PAGE_LIMIT_URI);
+        
     }
 }
