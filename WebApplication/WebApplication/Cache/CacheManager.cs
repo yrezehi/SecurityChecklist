640670923f246c703cf545eadd8876f8c6af19b0
unknown <yrezehi@gmail.com>
2023-10-05 21:45:44 -0700

    add: TerminateSessionEventHandler to cookie event

---
+++ WebApplication/WebApplication/Cache/CacheManager.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System.Collections.Concurrent;
+
+namespace Application.Cache
+{
+    public class CacheManager
+    {
+        private readonly ConcurrentDictionary<string, MemoryCache> Caches;
+
+        public CacheManager() =>
+            Caches = new ConcurrentDictionary<string, MemoryCache>();
+
+        private MemoryCache CreateMemeoryCacheInstance() =>
+            new MemoryCache(new MemoryCacheOptions());
+
+        public MemoryCache GetCache(string name)
+        {
+            if (Caches.ContainsKey(name))
+                return Caches[name];
+
+            throw new ArgumentException($"{name} is not initialized.");
+        }
+
+        public void CreateCache(string name)
+        {
+            if(!Caches.TryAdd(name, CreateMemeoryCacheInstance()))
+                throw new ArgumentException($"Was not able to create a cache instance for {name}");
+        }
+
+        public bool Contains(string name, string key) =>
+            this.GetCache(name).TryGetValue(key, out _);
+
+        public void Remove(string name, string key) =>
+            this.GetCache(name).Remove(key);
+
+        public void Set<T>(string name, string key, T value) =>
+            this.GetCache(name).Set(key, value);
+
+        public T? Get<T>(string name, string key) =>
+            this.GetCache(name).TryGetValue(key, out T? value) ? value : default;
+
+        public int Increment(string name, string key)
+        {
+            if (this.Contains(name, key))
+            {
+                int previousValue = this.Get<int>(name, key);
+                this.Set<int>(name, key, previousValue + 1);
+                return previousValue + 1;
+            }
+            return 1;
+        }
+
+    }
+}
---
+++ WebApplication/WebApplication/Events/Handlers/TerminateSessionEventHandler.cs
@@ -0,0 +1,37 @@
+﻿using Application.Cache;
+using System.Collections.Concurrent;
+
+namespace Application.Events.Handlers
+{
+    public class TerminateSessionEventHandler
+    {
+        private readonly CacheManager CacheManager;
+
+        private static string CACHE_NAME = "TERMINATED_SESSIONS";
+
+        public TerminateSessionEventHandler(CacheManager cacheManager)
+        {
+            CacheManager = cacheManager;
+            CacheManager.CreateCache(CACHE_NAME);
+        }
+
+        public void Terminate(HttpContext httpContext)
+        {
+            if(httpContext.Request.Cookies.Count > 0)
+            {
+                var requestCookie = httpContext.Request.Cookies.FirstOrDefault(cookie => cookie.Key.Contains(".AspNetCore."));
+                CacheManager.Set(CACHE_NAME, requestCookie.Value, DateTime.Now);
+            }
+        }
+
+        public bool IsTerminated(HttpContext httpContext)
+        {
+            if (httpContext.Request.Cookies.Count > 0)
+            {
+                var requestCookie = httpContext.Request.Cookies.FirstOrDefault(cookie => cookie.Key.Contains(".AspNetCore."));
+                return CacheManager.Contains(CACHE_NAME, requestCookie.Value);
+            }
+            return false;
+        }
+    }
+}
