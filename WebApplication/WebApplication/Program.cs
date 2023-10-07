8d65ce82a1fbd002a561280bc5d55be0cd2fe492
unknown <yrezehi@gmail.com>
2023-10-06 21:01:30 -0700

    setup: cookieauth

--- WebApplication/WebApplication/Program.cs
+++ WebApplication/WebApplication/Program.cs
@@ -4,6 +4,7 @@ using Application.Events.Handlers;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Application.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +20,18 @@ builder.Services.AddSingleton(typeof(FailedAttemptsEventHandler), typeof(FailedA
 builder.Services.AddSingleton(typeof(TerminateSessionEventHandler), typeof(TerminateSessionEventHandler));
 
 builder.Services.AddTransient(typeof(UserService), typeof(UserService));
+
+builder.Services.AddAuthentication(option =>
+{
+    option.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    option.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    option.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+}).Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+{
+    options.Cookie.HttpOnly = true; // OWASP-A01
+    options.ExpireTimeSpan = TimeSpan.FromHours(60); // OWASP-A01
+    options.SlidingExpiration = false; // OWASP-A01
+});
 
 var app = builder.Build();
 
@@ -37,8 +50,6 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
