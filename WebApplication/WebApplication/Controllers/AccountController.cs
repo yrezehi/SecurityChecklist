287f959cddda7cd3b838ff7504f79769d30ea3ce
unknown <yrezehi@gmail.com>
2023-10-10 23:00:21 -0700

    secuirty

---
+++ WebApplication/WebApplication/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Controllers
+{
+    [Route("[controller]")]
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> Logger;
+
+        public AccountController(ILogger<AccountController> logger) =>
+            Logger = logger;
+
+        [HttpGet("[action]")]
+        public IActionResult Login() =>
+            View();
+    }
+}
--- WebApplication/WebApplication/Controllers/HomeController.cs
+++ WebApplication/WebApplication/Controllers/HomeController.cs
@@ -6,27 +6,16 @@ namespace Application.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<HomeController> Logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        public HomeController(ILogger<HomeController> logger) =>
+            Logger = logger;
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
+        public IActionResult Index() =>
+            View();
+        
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        public IActionResult Error() =>
+            View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
