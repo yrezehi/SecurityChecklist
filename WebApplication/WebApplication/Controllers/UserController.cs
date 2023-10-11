5a86a1992b340c5908a949df350d8e74c1514c45
unknown <yrezehi@gmail.com>
2023-10-11 00:59:30 -0700

    fix: minor

---
+++ WebApplication/WebApplication/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Controllers
+{
+    [Route("[controller]")]
+    public class UserController : Controller
+    {
+        private readonly ILogger<UserController> Logger;
+
+        public UserController(ILogger<UserController> logger) =>
+            Logger = logger;
+
+        [HttpGet("[action]")]
+        public IActionResult Login() =>
+            View();
+
+        [HttpPost("[action]")]
+        public IActionResult Authenticate()
+        {
+
+            return View();
+        }
+    }
+}
